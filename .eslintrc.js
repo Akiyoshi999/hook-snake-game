78ebf999b655c87ade180412e7b8a8b0e0364e34
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-04-15 21:49:28 +0900

    自動補完の設定

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:react/recommended', 'eslint:recommended'],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+}
