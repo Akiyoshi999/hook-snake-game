0f5b617700dd5dc96a7bae6acfebf4bba22e80e2
Akiyoshi <zundokokiyoshi999@gmail.com>
2021-04-18 14:29:21 +0900

    ランダムにエサを表示

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,14 +2,36 @@ type Coordinate = {
   x: number
   y: number
 }
+// [{x:17,y:17}]
+// type snakeObject = object[]
 
-export const initFields = (fieledSize: number, initialPosition: Coordinate) => {
+const getFoodPosition = (
+  fieldSize: number,
+  excludes: Array<Coordinate>
+): Coordinate => {
+  for (;;) {
+    console.log(typeof excludes, excludes)
+    const x = Math.floor(Math.random() * (fieldSize - 2)) + 1
+    const y = Math.floor(Math.random() * (fieldSize - 2)) + 1
+    const conflict = excludes.some(
+      (item: Coordinate) => item.x === x && item.y === y
+    )
+    if (!conflict) {
+      return { x, y }
+    }
+  }
+}
+
+export const initFields = (fieledSize: number, snake: Coordinate) => {
   const fileds = []
   for (let i = 0; i < fieledSize; i++) {
     const cols = new Array(fieledSize).fill('')
     fileds.push(cols)
   }
-  fileds[initialPosition.y][initialPosition.x] = 'snake'
+  fileds[snake.y][snake.x] = 'snake'
+
+  const food = getFoodPosition(fieledSize, [snake])
+  fileds[food.x][food.y] = 'food'
 
   return fileds
 }
